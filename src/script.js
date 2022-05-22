f1a1ff045d93c4a6a298c831fdfea165daa07e35
thcheetah777 <thcheetah777@gmail.com>
2022-05-22 11:28:33 -0700

    Lots of stuff

--- src/script.js
+++ src/script.js
@@ -10,7 +10,38 @@ const elevator = new Elevator({
 });
 
 let backToTopButton = document.getElementById("backToTop");
+let parallaxImage = document.getElementById("thumbnail");
+let cards = document.querySelectorAll(".card");
+
+const cardsMoveOut = "500px";
+const cardsRotation = "10deg";
+const cardsTranslate = "150px";
+
 backToTopButton.onclick = () => {
   elevator.elevate();
-  console.log("L");
+}
+for (var i = 0; i < cards.length; i++) {
+  let card = cards[i];
+  card.onclick = () => {
+    if (card.className.includes("card1")) {
+      card.style.transform = `rotate(${cardsRotation}) translate(${cardsMoveOut})`;
+    } else if (card.className.includes("card3")) {
+      card.style.transform = `rotate(-${cardsRotation}) translate(-${cardsMoveOut})`;
+    } else {
+      card.style.transform = `rotate(0deg) translate(0px, ${cardsMoveOut})`;
+    }
+    setTimeout(function () {
+      for (var x = 0; x < cards.length; x++) {
+        cards[x].style.zIndex = 0;
+      }
+      if (card.className.includes("card1")) {
+        card.style.transform = `rotate(${cardsRotation}) translate(${cardsTranslate})`;
+      } else if (card.className.includes("card3")) {
+        card.style.transform = `rotate(-${cardsRotation}) translate(-${cardsTranslate})`;
+      } else {
+        card.style.transform = `rotate(0deg) translate(0px, 0px)`;
+      }
+      card.style.zIndex = 1;
+    }, 200);
+  }
 }
